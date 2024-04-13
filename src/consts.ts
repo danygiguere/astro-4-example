ffe711594e2d50717afb6b51dd83ac79cec315a6
Dany Giguere <danygiguere23@gmail.com>
2024-04-13 15:39:09 -0400

    minor refacto

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro i18n';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
-
 export const TOKEN = "token";
 export const PUBLIC_ROUTES = ["/", "/api/login", "/api/logout"];

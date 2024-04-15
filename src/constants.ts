52838d092242b839fdf621518a1b53d1315cef2c
Dany Giguere <danygiguere23@gmail.com>
2024-04-15 14:13:33 -0400

    getting ready for jwt implementation

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const TOKEN = "token";
+export const JWT_TOKEN = "token";
 export const PUBLIC_ROUTES = ["/", "/api/login", "/api/logout"];
--- src/pages/api/signin.ts
+++ src/pages/api/signin.ts
@@ -1,26 +1,35 @@
 import type { APIRoute } from "astro";
 import I18n from "../../i18n";
-
+import { SignJWT } from "jose";
+import { nanoid } from "nanoid";
+import { JWT_TOKEN } from "../../constants";
 
 export const POST: APIRoute = async ({ request }) => {
   const locale = request.headers.get("accept-language");
   const i18n = new I18n(locale);
-
-  const data = await request.formData();
-  const email = data.get("email");
-  const password = data.get("password");
-  if (!email || !email || !password) {
+  try {
+    const formData = await request.formData();
+    const response = await fetch(`http://localhost:8012/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept-Language": i18n.locale,
+      },
+      body: JSON.stringify(Object.fromEntries(formData.entries())),
+    });
     return new Response(
       JSON.stringify({
-        message: i18n.t("errors.missing-fields")
+        user: await response.json(),
+        message: i18n.t("validations.login-success"),
+      }),
+      { status: 200 }
+    );
+  } catch (e) {
+    return new Response(
+      JSON.stringify({
+        message: i18n.t("validations.login-error"),
       }),
       { status: 400 }
     );
   }
-  return new Response(
-    JSON.stringify({
-      message: i18n.t("errors.success")
-    }),
-    { status: 200 }
-  );
 };

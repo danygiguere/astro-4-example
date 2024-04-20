39632cdf739bd7dc51f2a8b35e2175bb3aff2567
Dany Giguere <danygiguere23@gmail.com>
2024-04-20 12:27:06 -0400

    refactored middleware

--- src/constants.ts
+++ src/constants.ts
@@ -2,16 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const TOKEN = "token";
-export const PUBLIC_ROUTES = [
-  "/en",
-  "/en/",
-  "/fr",
-  "/fr/",
-  "/en/signin",
-  "/en/signin/",
-  "/fr/signin",
-  "/fr/signin/",
-  "/api/signup",
-  "/api/signin",
-  "/api/signout",
+export const PROTECTED_ROUTES = [
+  "/en/about",
+  "/fr/about"
 ];
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,35 @@
 import { defineMiddleware } from "astro/middleware";
-import { TOKEN, PUBLIC_ROUTES } from "./constants";
+import { TOKEN, PROTECTED_ROUTES } from "./constants";
 import verifyAuth from "./auth";
 
-const secret = new TextEncoder().encode(import.meta.env.JWT_SECRET_KEY);
-
 export const onRequest = defineMiddleware(async (context, next) => {
-  if (PUBLIC_ROUTES.includes(context.url.pathname)) {
-    return next();
-  }
+  const cleanContextUrl = context.url.pathname.replace(/\/$/, "");
+  if (PROTECTED_ROUTES.includes(cleanContextUrl)) {
+    // I should move the verifyAuth into a auth middleware and apply that middleware on specific pages
+    const token = context.cookies?.get(TOKEN)?.value;
+    const validationResult = await verifyAuth(token);
 
-  const token = context.cookies?.get(TOKEN)?.value;
-  const validationResult = await verifyAuth(token);
-
-  switch (validationResult.status) {
+    switch (validationResult.status) {
     case "authorized":
       return next();
 
     case "error":
     case "unauthorized":
-      if (context.url.pathname.startsWith("/api/")) {
+      if (cleanContextUrl.startsWith("/api")) {
         return new Response(JSON.stringify({ message: validationResult.msg }), {
           status: 401,
         });
+      } else {
+        console.log("redirect to /signin");
+        return Response.redirect(
+          new URL(`/${context.currentLocale}/signin`, context.url)
+        );
       }
-      else {
-        return Response.redirect(new URL(`/${context.currentLocale}/signin`, context.url));
-      }
-
     default:
       return Response.redirect(
-        new URL(`/${context.currentLocale}`, context.url)
+        new URL(`/${context.currentLocale}/signin`, context.url)
       );
+    }
   }
+  return next();
 });
